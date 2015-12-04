5aa9e6e8328f55e4e96ac3f0ca5eb1c112264b02
Julian Duque <julianduquej@gmail.com>
2015-12-04 13:50:00 -0500

    update Node.js version to 4.2.3

--- upgrade-ready.js
+++ upgrade-ready.js
@@ -24,7 +24,7 @@ var urlJoin = require('url-join')
 var config = rc('upgrade-ready', {
   endpoint: 'http://upgrade-ready.nodesource.com/api/',
   verbose: false,
-  targetVersion: '4.2.2',
+  targetVersion: '4.2.3',
   path: path.resolve('.')
 })
 
