d7c6fa887c217f528fe9bd4aa123af7f39198156
Julian Duque <julianduquej@gmail.com>
2016-02-15 12:04:39 -0500

    Update default Node.js version to v4.3.0

--- upgrade-ready.js
+++ upgrade-ready.js
@@ -24,7 +24,7 @@ var urlJoin = require('url-join')
 var config = rc('upgrade-ready', {
   endpoint: 'http://upgrade-ready.nodesource.com/api/',
   verbose: false,
-  targetVersion: '4.2.6',
+  targetVersion: '4.3.0',
   path: path.resolve('.')
 })
 
